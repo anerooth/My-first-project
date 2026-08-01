aadb4d75332d0528c11d60bb4fa92350acee4ea3
Anerooth P.K <anerooth@Anerooths-MacBook-Air.local>
2026-08-01 20:20:33 +0530

    i made delete post through databsae

--- App/main.py
+++ App/main.py
@@ -74,11 +74,11 @@ def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index(id)
-    if index == None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s""",(int(id),))
+    conn.commit()
+    if id == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"your post with id:{id} was not found")
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
